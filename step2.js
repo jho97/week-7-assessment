d2eb9d426e8064c5ab314de7566a38ddcc225621
Jonathan Ho <ho.jonathan97@gmail.com>
2022-05-06 14:17:58 -0500

    last one

--- step2.js
+++ step2.js
@@ -21,3 +21,15 @@
 // }
 
 // console.log(hasUniqueChars("monday"))
+
+// const isPangram = sentence => {
+//     // wanted to do something where it could match the index values inside my array, then I thought it would take some for loops. Then I found something on the internet about regex
+//     // let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//     let newSentence = sentence.toLowerCase().match(/[a-z]/g)
+
+//     let alphabet = [...new Set(newSentence)]
+
+//     return alphabet.length === 26
+// }
+
+// console.log(isPangram("I like cats, but not mice"))
